ff8a2ec9860cc0d489486d25e43d9117d42f7ea2
ben <ben@tradev-group.com>
2025-03-03 13:55:00 +0800

    fix redundant code

--- main.go
+++ main.go
@@ -5,20 +5,20 @@ import "SmilePay-Demo-Golang/handle"
 func main() {
 
 	//Step one, generate RSA key
-	//handle.GenerateRSA()
+	handle.GenerateRSA()
 
 	//Step two, generate token
-	//handle.AccessToken()
+	handle.AccessToken()
 
 	//Step three, call pay interface
 	handle.PayIn()
 
 	//step four, call inquiry interface
-	//handle.InquiryPaymentMethod()
+	handle.InquiryPaymentMethod()
 
 	//step five, call inquiry account interface
-	//handle.InquiryAccount()
+	handle.InquiryAccount()
 
 	//step six, call payout interface
-	//handle.PayOut()
+	handle.PayOut()
 }
